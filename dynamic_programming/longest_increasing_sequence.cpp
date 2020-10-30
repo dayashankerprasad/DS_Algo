a3ebb99c623a1219f877721ef8c68002d8bcd979
Daya Shanker Prasad <mail.dsp@gmail.com>
2020-10-30 06:29:54 +0530

    Length of longest increasing subsequence

--- dynamic_programming/longest_increasing_sequence.cpp
+++ dynamic_programming/longest_increasing_sequence.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//O(n^2)
 uint32_t longestIncreasingSequenceLength(vector<int32_t> const &array)
 {
 	vector<int32_t> table(array.size() * array.size(), 1);
@@ -21,11 +22,35 @@ uint32_t longestIncreasingSequenceLength(vector<int32_t> const &array)
 	return maxLength;
 }
 
+//O(nlogn)
+uint32_t longestIncreasingSequenceLength_2(vector<int32_t> const &array)
+{
+	vector<int32_t> seq;
+	seq.push_back(array[0]);
+	
+	for (uint32_t i = 1; i < array.size(); ++i)
+	{
+		if (array[i] > seq.back())
+			seq.push_back(array[i]);
+		else
+		{
+			auto index = lower_bound(begin(seq), end(seq), array[i]) - begin(seq);
+			seq[index] = array[i];
+		}
+	}
+	
+	return seq.size();
+}
+
 int32_t main()
 {
 	cout << longestIncreasingSequenceLength({1, 2, 3, 4, 5, 3, 5, 6}) << endl;
 	cout << longestIncreasingSequenceLength({1, 2, 1, 2}) << endl;
 	cout << longestIncreasingSequenceLength({10, 9, 2, 3, 4, 5, 6}) << endl;
+
+	cout << longestIncreasingSequenceLength_2({1, 2, 3, 4, 5, 3, 5, 6}) << endl;
+	cout << longestIncreasingSequenceLength_2({1, 2, 1, 2}) << endl;
+	cout << longestIncreasingSequenceLength_2({10, 9, 2, 3, 4, 5, 6}) << endl;
 
 	return 0;
 }
